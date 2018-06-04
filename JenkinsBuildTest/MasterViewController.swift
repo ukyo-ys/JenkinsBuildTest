c4c27fa94a10354b20c05607e482d3704a6bdc5d
Yoshida Ukyo <yoshida.ukyou.f@cti.co.jp>
2018-06-04 09:13:08 +0900

    commit5

--- JenkinsBuildTest/MasterViewController.swift
+++ JenkinsBuildTest/MasterViewController.swift
@@ -17,7 +17,7 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        title = "変更４"
+        title = "変更５"
         
         navigationItem.leftBarButtonItem = editButtonItem
 
